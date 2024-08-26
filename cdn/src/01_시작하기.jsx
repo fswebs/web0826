745223d1eceaac77508091aa859a4c1fdfb795b7
wi <whyeil29@gmail.com>
2024-08-26 18:12:34 +0900

    0826

--- cdn/src/01_시작하기.jsx
+++ cdn/src/01_시작하기.jsx
@@ -16,38 +16,30 @@ function Hello() {
     return (
         // JSX의 태그 끝에는 ; 쓰지 않는다.
         // return 안의 root 태그는 1개 이어야 한다.
-        <div style={{ margin: "12px" }}>
+        <div className="card_list">
             {/* XML에서 빈 요소는 끝에 <img /> */}
             <div className="card_img">
-              <img
-                  src="https://img.megabox.co.kr/SharedImg/2024/07/03/QaslTt607PkNH8mPWm6ZUH3UwDx2bHoH_420.jpg"
-                  alt="사랑의 하츄핑"
-              />
+                <img
+                    src="https://img.megabox.co.kr/SharedImg/2024/07/03/QaslTt607PkNH8mPWm6ZUH3UwDx2bHoH_420.jpg"
+                    alt="사랑의 하츄핑"
+                />
             </div>
             <div>
                 {/* {{}} -> 바깥쪽 괄호는 표현식 */}
                 {/* {{}} -> 안쪽 객체 괄호 */}
                 {/* 객체로 스타일을 지정한다. */}
                 {/* 속성명은 카멜표기법으로 쓴다. 예) borderRadius 또는 "border-radius" */}
-                <span
-                    style={{
-                        padding: "0 4px",
-                        borderRadius: "3px",
-                        background: "#229C56",
-                    }}
-                >
-                    All
-                </span>
+                <span className="card_age">All</span>
                 {/* class -> className 으로 기록한다.*/}
                 <h3 className="card_tit">사랑의 하츄핑</h3>
             </div>
             <div>
-                <span>예매율 1%</span>
-                <span>개봉일 2024.08.07</span>
+                <span className="card_rate">예매율 1%</span>
+                <span className="card_date">개봉일 2024.08.07</span>
             </div>
-            <div>
-                <button>❤ 487</button>
-                <button>예매</button>
+            <div className="card_btn">
+                <button>487</button>
+                <a href="#">예매</a>
             </div>
         </div>
     );
