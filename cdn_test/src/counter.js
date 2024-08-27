0c4a8de6829c62d17bbe68ad842c80138febc1e0
wi <whyeil29@gmail.com>
2024-08-27 21:03:44 +0900

    0827

---
+++ cdn_test/src/counter.js
@@ -0,0 +1,7 @@
+const heartBtn = document.querySelectorAll("button");
+for(const x of heartBtn){
+    x.innerText = 0;
+    x.onclick = ()=>{
+        x.innerText = parseInt(x.innerText) + 1;
+    };
+}
